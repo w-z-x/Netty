6102786eb1801acc37f8fe006dd2f931d15c21de
zhxwu <zhxwu@hotmail.com>
2019-12-11 22:22:32 +0800

    简单的Netty客户端和服务器

---
+++ ServerClient/EchoServer/src/main/java/server/EchoServer.java
@@ -0,0 +1,56 @@
+package server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+import java.net.InetSocketAddress;
+
+public class EchoServer {
+    private final int port;
+
+    private EchoServer(int port) {
+        this.port = port;
+    }
+
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.err.println("Usage: " + EchoServer.class.getSimpleName() + " <port>");
+            return;
+        }
+        int port = Integer.parseInt(args[0]);
+        new EchoServer(port).start();
+    }
+
+    private void start() {
+        final EchoServerHandler echoServerHandler = new EchoServerHandler();
+        EventLoopGroup eventLoopGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(eventLoopGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .localAddress(new InetSocketAddress(port))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        protected void initChannel(SocketChannel ch) {
+                            ch.pipeline().addLast(echoServerHandler);
+                        }
+                    });
+
+            ChannelFuture f = b.bind().sync();
+            f.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                eventLoopGroup.shutdownGracefully().sync();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
