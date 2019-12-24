070edab9fe48a04d3d838d2560ab02d1c7902aae
zhxwu <zhxwu@hotmail.com>
2019-12-24 23:55:35 +0800

    增加GET、POST

---
+++ SimpleHttpServer/src/main/java/SimpleHttpServer.java
@@ -0,0 +1,93 @@
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.net.URLDecoder;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SimpleHttpServer {
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/hello", new HelloHandler());
+        server.createContext("/get", new GetHandler());
+        server.createContext("/post", new PostHandler());
+        server.start();
+    }
+
+    static class HelloHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange httpExchange) throws IOException {
+            httpExchange.sendResponseHeaders(200, 0);
+            OutputStream os = httpExchange.getResponseBody();
+            String response = "hello, world";
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+
+    static class GetHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange httpExchange) throws IOException {
+            String queryString = httpExchange.getRequestURI().getQuery();
+            Map<String, String> map = str2Map(queryString);
+            Headers responseHeaders = httpExchange.getResponseHeaders();
+            responseHeaders.set("Content-Type", "text/html;charset=utf-8");
+            httpExchange.sendResponseHeaders(200, 0);
+            OutputStream os = httpExchange.getResponseBody();
+            String response = "hello, " + map.getOrDefault("name", "nobody");
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+
+    private static Map<String, String> str2Map(String queryString) {
+        Map<String, String> result = new HashMap<>();
+        if (queryString == null || queryString.trim().length() == 0) {
+            return result;
+        }
+        final String[] items = queryString.split("&");
+        Arrays.stream(items).forEach(item -> {
+            final String[] keyAndVal = item.split("=");
+            if (keyAndVal.length == 2) {
+                try {
+                    final String key = URLDecoder.decode(keyAndVal[0], "utf-8");
+                    final String val = URLDecoder.decode(keyAndVal[1], "utf-8");
+                    result.put(key, val);
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        return result;
+    }
+
+    static class PostHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange httpExchange) throws IOException {
+            String postString = is2String(httpExchange.getRequestBody());
+            Map<String, String> map = str2Map(postString);
+            Headers responseHeaders = httpExchange.getResponseHeaders();
+            responseHeaders.set("Content-Type", "text/html;charset=utf-8");
+            httpExchange.sendResponseHeaders(200, 0);
+            OutputStream os = httpExchange.getResponseBody();
+            String response = "hello, " + map.getOrDefault("name", "nobody");
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+
+    private static String is2String(InputStream is) throws IOException {
+        ByteArrayOutputStream result = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = is.read(buffer)) != -1) {
+            result.write(buffer, 0, length);
+        }
+        return result.toString("UTF-8");
+    }
+}
